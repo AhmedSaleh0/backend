80535bbc93f96ec0fcfecf57be927a58174d27c9
Ahmed Saleh <asaleh@autodata.ae>
2024-06-14 02:30:27 +0300

    fixing inspire like 0.4

--- app/Http/Middleware/OptionalAuth.php
+++ app/Http/Middleware/OptionalAuth.php
@@ -3,29 +3,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Passport\TokenRepository;
-use Laravel\Passport\Token;
 
 class OptionalAuth
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-        if ($token = $request->bearerToken()) {
-            $tokenId = optional((new TokenRepository)->find($token))->id;
-            if ($tokenId) {
-                $user = optional(Token::find($tokenId))->user;
-                if ($user) {
-                    Auth::setUser($user);
-                }
-            }
+        // Attempt to authenticate the user using the 'api' guard
+        if ($request->bearerToken()) {
+            Auth::shouldUse('api');
+            Auth::onceBasic('email') ?: Auth::guard('api')->user();
         }
 
         return $next($request);
