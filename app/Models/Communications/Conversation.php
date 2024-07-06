b0ffe741bca5f8a2982b02e26ec6fe6edb43d751
Ahmed Saleh <asaleh@autodata.ae>
2024-07-06 22:08:30 +0400

    creating messaging and conversations 0.7

--- app/Models/Communications/Conversation.php
+++ app/Models/Communications/Conversation.php
@@ -10,6 +10,7 @@ class Conversation extends Model
 {
     use HasFactory;
 
+    protected $table = 'conversations';
     protected $fillable = ['user_one_id', 'user_two_id'];
 
     public function userOne()
